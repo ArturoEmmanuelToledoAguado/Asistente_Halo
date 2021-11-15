9af9ba4c602b33862dcb1b4174be0dd44bfe3d3e
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2021-11-14 20:42:06 -0600

    Primera pergunta

--- main.py
+++ main.py
@@ -12,17 +12,32 @@ engine.setProperty('voice', voices[0].id)
 engine.say("Hola soy rubicon, un asistente virtual para la historia de halo.")
 engine.runAndWait()
 
+#Habla
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:#Usamos el microfono como fuente
-        print("Escuchando...")
-        voice = listener.listen(source)#Escucha
-        rec = listener.recognize_google(voice)#Pasa a texto lo escuchado
-        rec = rec.lower()
-        if name in rec:
-            talk(rec)
-except:
-    pass
+#Escucha
+def listen():
+    try:
+        with sr.Microphone() as source:#Usamos el microfono como fuente
+            print("Escuchando...")
+            voice = listener.listen(source)#Escucha
+            rec = listener.recognize_google(voice)#Pasa a texto lo escuchado
+            rec = rec.lower()
+            if name in rec:
+                print(rec)
+    except:
+        pass
+    return rec
+
+def run():
+    rec = listen()
+    if 'que' in rec or 'halo' in rec:
+        talk('En el mundo real, Halo es el nombre de un videojuego creado por la empresa BUNGIE estudios y estrenado el '
+             '15 de noviembre del 2001 con la primer entrega de la franquicia llamada Halo: Combat Evolved; En el Lore '
+             'del juego, Halo es el nombre de un sistema de armamento neurofísico creado por la raza de los Forerunners '
+             'cuyo objetivo es eliminar la amenaza Flood de la galaxia.')
+    if
+
+run()
